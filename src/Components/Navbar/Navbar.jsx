501d1adf67a37eaf56b625d05a891bbef2cf3a1d
Robert597 <robertseun1@gmail.com>
2023-08-24 21:08:47 +0100

    mobile-nav

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ import Logo from "/vision pro 2/logo.png";
 const Navbar = () => {
 
   return (
+    <div className="navv">
     <div className='NavbarContainer'>
         <div className="NavbarLogo">
             <img src={Logo} alt="apple-Logo"/>
         </div>
-        <MobileNav/>
         <ul className="Navbar-Link-Content">
             <li><a href="#" data-replace="Home">Home</a></li>
             <li><a href="#" data-replace="Design">Design</a></li>
@@ -17,6 +17,8 @@ const Navbar = () => {
             <li><a href="#" data-replace="Security">Security</a></li>
         </ul>
         <button className='navbarButton'>Notify Me</button>
+    </div>
+    <MobileNav/>
     </div>
   )
 }
